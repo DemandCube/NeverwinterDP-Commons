23a3abf8814699a9e5a5e6065bb293200587d0d5
Tuan Nguyen <tuan08@gmail.com>
2014-06-17 19:38:04 +0700

    fix sleep time

--- hadoop-framework/src/main/java/com/neverwinterdp/hadoop/yarn/hello/HelloWorker.java
+++ hadoop-framework/src/main/java/com/neverwinterdp/hadoop/yarn/hello/HelloWorker.java
@@ -3,6 +3,5 @@ package com.neverwinterdp.hadoop.yarn.hello;
 public class HelloWorker {
   static public void main(String[] args) throws Exception {
     System.out.println("Hello Worker!!!");
-    Thread.sleep(100000000);
   }
 }
