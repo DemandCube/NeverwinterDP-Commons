614fb18826683b879df199e197bda25ea7c70a7c
KingOfPoptart <richard.c.duarte@gmail.com>
2014-08-30 12:27:15 +0100

    Fixing port conflict on Jenkins

--- netty/src/test/java/com/neverwinterdp/netty/http/webapp/WebAppUnitTest.java
+++ netty/src/test/java/com/neverwinterdp/netty/http/webapp/WebAppUnitTest.java
@@ -20,10 +20,14 @@ import com.neverwinterdp.netty.http.client.AsyncHttpClient;
  */
 public class WebAppUnitTest {
   private HttpServer server ;
+  private int port =8181;
   
   @Before
   public void setup() throws Exception {
     server = new HttpServer();
+    Map<String, String> props = new HashMap<String, String>();
+    props.put("port", Integer.toString(port));
+    server.configure(props);
     server.add("/hello", new HelloHandler());
     new Thread() {
       public void run() {
@@ -45,7 +49,7 @@ public class WebAppUnitTest {
   @Test
   public void testHello() throws Exception {
     DumpResponseHandler handler = new DumpResponseHandler() ;
-    AsyncHttpClient client = new AsyncHttpClient ("127.0.0.1", 8080, handler) ;
+    AsyncHttpClient client = new AsyncHttpClient ("127.0.0.1", port, handler) ;
     client.get("/hello");
     
     Thread.sleep(1000);
